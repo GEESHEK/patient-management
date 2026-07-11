7ae58b2366af02e4de8ad0b5b7a687a90483c8e2
Gee Chong Shek <geeshek95@outlook.com>
2026-07-11 11:46:54 +0100

    Updated post request to return the correct http code

--- patient-service/src/main/java/com/gee/patientservice/contoller/PatientController.java
+++ patient-service/src/main/java/com/gee/patientservice/contoller/PatientController.java
@@ -4,10 +4,11 @@ import com.gee.patientservice.dto.PatientRequestDto;
 import com.gee.patientservice.dto.PatientResponseDTO;
 import com.gee.patientservice.service.PatientService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,7 +30,14 @@ public class PatientController {
     @PostMapping
     public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDto patientRequestDto) {
         PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(patientResponseDTO);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(patientResponseDTO.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(patientResponseDTO);
     }
 
 }
